d110e5c367457abdde8c653c8e10b14154364d5f
Harold GR <harold@girres.com.co>
2024-12-04 10:17:31 +0100

    Changes

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import { getPostsPromoted } from '@/services/strapi';
 // Components.
 import Algolia from '@/components/Algolia';
 import PostPromoted from '@/components/Blog/PostPromoted';
+import Image from 'next/image';
 
 // Revalidate time for the page // 10 minutes
 export const revalidate = 600;
@@ -16,8 +17,18 @@ export default async function Home() {
   return (
     <main className='main-content'>
       <h1 className='hidden'>HUB Herragro</h1>
-      <div className='px-5 relative w-full h-auto bg-[url("/images/v2/bg.gif")] bg-no-repeat bg-cover bg-center object-cover'>
-        <Algolia />
+      <div>
+        <div className='relative min-h-[800px] w-full'>
+          <Image
+            src='/images/v2/bg.gif'
+            alt='HUB Herragro'
+            fill
+            className='object-cover object-center z-10'
+          />
+          <div className='absolute h-full w-full z-20 px-5'>
+            <Algolia />
+          </div>
+        </div>
       </div>
       <div className='container space-y-2'>
         <PostPromoted posts={posts} />
